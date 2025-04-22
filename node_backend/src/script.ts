8db914fdd2aa9aec72c0580885d885d6d9bbea32
Ishan200207 <168297757+Ishan200207@users.noreply.github.com>
2025-04-22 11:06:58 +0530

    Initial Commit with Websocket Server Initialization

---
+++ node_backend/src/script.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import http from "http";
+import cors from "cors";
+import dotenv from "dotenv";
+import { WebSocket, WebSocketServer } from "ws";
+
+dotenv.config();
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server })
+
+app.use(cors());
+app.use(express.json());
+
+//Create a WebSocket Connection
+wss.on("connection", (ws:WebSocket)=>{
+    console.log("New Client Connected");
+
+    ws.on("message", (data)=>{
+        console.log("Received a Message from the Client: " + data);
+        wss.clients.forEach((client)=>{
+            if(client !== ws && client.readyState == WebSocket.OPEN){
+                client.send(data);
+            }
+        });
+    });
+
+    ws.on("close", ()=>{
+        console.log("Client Disconnected!");
+    });
+});
+
+const PORT = process.env.PORT
+
+server.listen(PORT, ()=>{
+    console.log(`Server is Running On Port: ${PORT}`);
+});
